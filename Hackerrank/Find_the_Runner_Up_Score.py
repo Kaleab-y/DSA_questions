96c68c25f4d4704ed050518fcc911ffc6ed0a4d7
Kaleab-y <Kaleabyohannes135@gmail.com>
2024-07-24 15:06:17 +0300

    Update Find_the_Runner_Up_Score.py

--- Hackerrank/Find_the_Runner_Up_Score.py
+++ Hackerrank/Find_the_Runner_Up_Score.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     n = int(input())
     arr = list(map(int, input().split()))
-    SortedUniqueScore = sorted(set(arr), reverse=True)
-    RunnerUp = SortedUniqueScore[1]
+    SortedUniqueScore = sorted(set(arr))
+    RunnerUp = SortedUniqueScore[-2]
     print(RunnerUp)
 
